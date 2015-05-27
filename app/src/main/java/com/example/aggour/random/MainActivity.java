8595f75420ecd75cb799782737f52e09b51518ed
fuqthislag <fuqthislag@users.noreply.github.com>
2015-05-27 16:06:58 +0300

    0.0.4

--- app/src/main/java/com/example/aggour/random/MainActivity.java
+++ app/src/main/java/com/example/aggour/random/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.aggour.random;
 
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -9,7 +10,11 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class MainActivity extends Activity {
 
     EditText editText, editText2, editText3, editText4;
     TextView textView, textView2;
@@ -36,19 +41,12 @@ public class MainActivity extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                RNG RngObject = new RNG(
-                        editText.getText().toString(),
-                        editText2.getText().toString(),
-                        checkBox.isChecked(),
-                        MainActivity.this);
-                RNG RngObject2 = new RNG(
-                        editText3.getText().toString(),
-                        editText4.getText().toString(),
-                        checkBox.isChecked(),
-                        MainActivity.this);
-
-                textView.setText(RngObject.result);
-                textView2.setText(RngObject2.result);
+                new ThreadMe().execute(
+                        editText.getText().toString() + "",
+                        editText2.getText().toString() + "",
+                        editText3.getText().toString() + "",
+                        editText4.getText().toString() + "",
+                        checkBox.isChecked());
             }
         });
 
@@ -64,6 +62,96 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         });
+    }
 
+    protected class ThreadMe extends AsyncTask<Object, String, Void> {
+
+        @Override
+        protected Void doInBackground(Object... params) {
+            String editText = (String) params[0];
+            String editText2 = (String) params[1];
+            String editText3 = (String) params[2];
+            String editText4 = (String) params[3];
+            Boolean checkBox = (Boolean) params[4];
+
+            int i, many, max, many2, max2;
+            String result = "", result2 = "", correct = "", correct2 = "";
+
+            if (editText == "" || editText2 == "") {
+                many = 1;
+                max = 1;
+            } else {
+                many = Integer.parseInt(editText);
+                max = Integer.parseInt(editText2);
+                if (checkBox && max < many) {
+                    max = many;
+                    correct = String.valueOf(max);
+                    publishProgress(result, result2, correct, correct2);
+                }
+            }
+
+            if (editText3 == "" || editText4 == "") {
+                many2 = 1;
+                max2 = 1;
+            } else {
+                many2 = Integer.parseInt(editText3);
+                max2 = Integer.parseInt(editText4);
+                if (checkBox && max2 < many2) {
+                    max2 = many2;
+                    correct2 = String.valueOf(max2);
+                    publishProgress(result, result2, correct, correct2);
+                }
+            }
+
+            correct = String.valueOf(max);
+            correct2 = String.valueOf(max2);
+
+            try {
+                if (checkBox) {
+                    ArrayList<Integer> list = new ArrayList<>();
+                    for (i = 1; i <= max; i++)
+                        list.add(i);
+                    Collections.shuffle(list);
+                    for (i = 0; i < many; i++) {
+                        result += list.get(i) + " ";
+                        Thread.sleep(17);
+                        publishProgress(result, result2, correct, correct2);
+                    }
+                    ArrayList<Integer> list2 = new ArrayList<>();
+                    for (i = 1; i <= max2; i++)
+                        list2.add(i);
+                    Collections.shuffle(list2);
+                    for (i = 0; i < many2; i++) {
+                        result2 += list2.get(i) + " ";
+                        Thread.sleep(17);
+                        publishProgress(result, result2, correct, correct2);
+                    }
+                } else {
+                    Random rand = new Random();
+                    for (i = 0; i < many; i++) {
+                        result += 1 + rand.nextInt(max) + " ";
+                        Thread.sleep(17);
+                        publishProgress(result, result2, correct, correct2);
+                    }
+                    for (i = 0; i < many2; i++) {
+                        result2 += 1 + rand.nextInt(max2) + " ";
+                        Thread.sleep(17);
+                        publishProgress(result, result2, correct, correct2);
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            textView.setText(values[0]);
+            textView2.setText(values[1]);
+            editText2.setText(values[2]);
+            editText4.setText(values[3]);
+        }
     }
 }
